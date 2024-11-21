cb1a6684091c30546d8af7757f88f7ad0bdf7c4c
alvarengayas <00001111190306sp@al.educacao.sp.gov.br>
2024-11-21 10:48:27 -0300

    Add files via upload

---
+++ ferramentas de estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'o que é html?',
+    'é o bloco construção mais básico da web'
+)
+
+criaCartao(
+    'Matemática',
+    'qual a prova real da multiplicação?',
+    'divisão'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função afim?',
+    ' qualquer função que possua a lei de formação y = ax + b, sendo a e b números reais e a diferente de zero.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz dia em Inglês?',
+    'dia em ingles é day(dei)'
+)
